a5d3264e798ba4e42e836cf40fea58aee61833d5
Oleg Aculov <oaculov@gmail.com>
2024-10-30 00:13:31 -0400

    negative test created

--- src/specs/user/login.spec.ts
+++ src/specs/user/login.spec.ts
@@ -6,33 +6,103 @@ const request = supertest("http://localhost:8001/api/v1")
 
 describe('LOGIN', () => {
 
-    describe('positive testing', () => {
+    describe.skip('positive testing', () => {
 
         it('should login user', async () => {
 
-            const res = await signUp(user);
-            // const loginRes = await request.post("/users/login")
+            //const loginRes = await request.post("/users/login")
             //     .send({
             //         "email": user.email,
             //         "password": user.password,
             //     }).expect(200);
 
+            const signUpRes = await signUp(user);
+            expect(signUpRes.body.status).toBe("success");
             const loginRes = await logIn({
                 "email": user.email,
                 "password": user.password,
             })
+            //console.log(loginRes.body);
+            expect(loginRes.body.status).toBe("success");
+            expect(loginRes.body.token).toBeDefined();
+            expect(loginRes.body.data.user.name).toBe(user.name);
         });
 
     });
 
     describe('negative tests', () => {
 
-        it("get error when trying login without password");
-// use all other methods
-        it("get error when trying login without username");
 
-        it("get error when trying login with wrong password");
-      //as well use hooks .then() .catch()  can't use it with async await
-        it("get error when trying login with wrong username");
+        it("get error when trying login without password", async () => { // using async await
+            const signupRes = await request.post("/users/signup").send(user).expect(201);
+            const loginRes = await request.post("/users/login").send({
+                email: user.email
+            }).expect(400);
+            expect(loginRes.body.message).toBe("Please provide email and password!");
+
+        });
+
+        it("get error when trying login without username", () => { // using hooks .then()
+
+             request.post("/users/signup").send(user).expect(201);
+
+             request.post("/users/login")
+                 .send({
+                     password: user.password
+                 })
+                 .then( el => {
+                     expect(el.body.status).toBe("fail");
+                     expect(el.body.message).toBe("Please provide email and password!");
+                })
+
+        });
+
+         it("get error when trying login with wrong password", (done) => {
+
+            request.post("/users/signup").send(user).expect(201);
+                    // .end( (err, res ) => {
+                    //     if(err) return done(err);
+                    //     expect(res.body.status).toBe("success");
+                    //     return done();
+                    // });
+
+             request.post("/users/login")
+                     .send({
+                         email: user.email,
+                         password: "wrongpass"
+                     })
+                    .expect(401)
+                    .end( (err, res) => {
+                        if(err) return done(err);
+                        expect(res.body.status).toBe("fail");
+                        expect(res.body.message).toBe("Incorrect email or password");
+                        return done();
+                    })
+
+         });
+
+       it("get error when trying login with wrong username",  () => {
+
+            request.post("/users/signup").send(user).expect(201);
+
+            return new Promise((resolve, reject) => {
+                request
+                    .post("/users/login")
+                    .send({
+                          email: "wrongemail",
+                          password: user.password
+                    })
+                    .expect(401)
+                    .end((err, res) => {
+                        if(err) console.log(err);
+                        expect(res.body.status).toBe("fail");
+                        expect(res.body.message).toBe('Incorrect email or password');
+                        return resolve(res);
+                    })
+           })
+
+       });
+
+
     });
 });
