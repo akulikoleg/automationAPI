cb6992d35540c1c3d63c71577c64966bf0a63162
akulikoleg <169015640+akulikoleg@users.noreply.github.com>
2024-10-30 00:15:16 -0400

    negative test created (#3)

--- src/data/helpers.ts
+++ src/data/helpers.ts
@@ -5,7 +5,7 @@ const request = supertest("http://localhost:8001/api/v1");
 
 // export async function signUp(user: string | object | undefined){
 //     await request.post("/users/signup").send(user);
-// }git status
+// }
 
 
 export async function signUp(user: string | object | undefined): Promise<any> {
@@ -21,7 +21,18 @@ export async function signUp(user: string | object | undefined): Promise<any> {
 
 }
 
-export async function  logIn(user: object){
-    await request.post("/users/login").send(user);
+export async function  logIn( user: object ): Promise<any> {
+
+    //await request.post("/users/login").send(user);
+
+    return new Promise( (resolve, reject) => {
+        request.post("/users/login")
+            .send(user)
+            .end( (err, res) => {
+                if (err) return reject(err);
+                return resolve(res);
+            })
+    })
+
 }
 
